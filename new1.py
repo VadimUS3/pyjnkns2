efc5023cb563a9f281244af08d67f90d952da1a3
VadimUS3 <149802354+VadimUS3@users.noreply.github.com>
2023-11-16 19:42:31 +0200

    Update new1.py

--- new1.py
+++ new1.py
@@ -2,5 +2,6 @@ print("!!!!! !!!!!!   Hello JENKINS")
 print("!!!!! !!!!!!   Hello AUTO BUILD2")
 print("!!!!! !!!!!!   Hello AUTO BUILD2")
 print("!!!!! !!!!!!   Hello AUTO BUILD3")
+print("!!!!! !!!!!!   Hello AUTO BUILD4")
 
 print("!!!!! !!!!!!   Hello JENKINS BUILD3")
